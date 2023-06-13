adeaea6da171f1f1f23ab5654d5d5eef0f7bfd25
snowcying <347025712@qq.com>
2023-06-13 20:39:00 +0800

    项目初始结构

---
+++ mail-product/src/test/java/com/cxy/mail/lproduct/MailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy.mail.lproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
