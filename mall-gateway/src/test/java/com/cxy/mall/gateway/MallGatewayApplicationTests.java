e5feea27a10af155e7dfae957684ba173008f581
snowcying <347025712@qq.com>
2023-06-27 15:47:51 +0800

    gateway

---
+++ mall-gateway/src/test/java/com/cxy/mall/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cxy.mall.gateway;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
