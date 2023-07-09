648402bae70b87501c210d0e72721b737bee7c38
snowcying <347025712@qq.com>
2023-07-09 14:59:36 +0800

    测试上传模块

--- mall-product/src/test/java/com/cxy/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/cxy/mall/product/MallProductApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cxy.mall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cxy.mall.product.entity.BrandEntity;
 import com.cxy.mall.product.service.BrandService;
@@ -9,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -18,6 +23,21 @@ class MallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    private OSS ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+//        String endpoint = "oss-cn-chengdu.aliyuncs.com";
+//        String accessKeyId="LTAI5t7PT1Sf7STnkYSf7bZC";
+//        String accessKeySecret="3yaKsySEPS1wfTO3jY9Hn5l6wXwQxd";
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\chenxinyi\\Pictures\\Saved Pictures\\2.JPG");
+        ossClient.putObject("gulimall-snowcying","bug3.JPG",inputStream);
+        ossClient.shutdown();
+        System.out.println("上传完成...");
+    }
 
     @Test
     void contextLoads() {
