e9903c7cb4d8e0eff58f33dd374a61d19676d824
snowcying <347025712@qq.com>
2023-08-01 23:36:34 +0800

    [elasticSearch]p128

--- mall-search/src/test/java/com/cxy/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/cxy/mall/search/MallSearchApplicationTests.java
@@ -1,13 +1,70 @@
 package com.cxy.mall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.cxy.mall.search.config.MallESConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
 @SpringBootTest
-class MallSearchApplicationTests {
+public class MallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient client;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println(client);
+    }
+
+    @Test
+    public void searchData() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("bank");
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+        System.out.println(sourceBuilder.toString());
+//        sourceBuilder.from();
+//        sourceBuilder.size();
+        searchRequest.source(sourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, MallESConfig.COMMON_OPTIONS);
+        System.out.println(searchResponse.toString());
+
+    }
+
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName", "zhangsan", "age", 18, "gender", "男");
+        User user = new User();
+        user.setUserName("shangsan");
+        user.setAge(18);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        IndexResponse index = client.index(indexRequest, MallESConfig.COMMON_OPTIONS);
+
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
 }
